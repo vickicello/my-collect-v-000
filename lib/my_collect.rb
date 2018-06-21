519b6e6e2f2fadb728d50edc2b09418e9e0c4abd
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 15:57:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,9 +8,12 @@ def my_collect(collection)
   collection
 end
 
-my_collect(collection) do |name|
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+my_collect(array) do |name|
   name.split(" ").first
 end
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 my_collect(collection) do |lang|
   lang.upcase
