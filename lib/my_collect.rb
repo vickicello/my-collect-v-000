fc26cc9d6d52f4d2f316e91c527529c556bdbe34
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 15:50:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
 def my_collect(collection)
   i = 0
   array = []
+  collection do |name|
+    name.split(" ").first
+  end
   while i < collection.length
     array << yield(collection[i])
     i += 1
