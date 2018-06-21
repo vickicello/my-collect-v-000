496d571bffe32773a845e220222eafeef17777bc
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 15:59:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,9 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
-  array = []
-  while i < collection.length
-    array << yield(collection[i])
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
     i += 1
   end
   collection
-end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(array) do |name|
-  name.split(" ").first
-end
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-
-my_collect(collection) do |lang|
-  lang.upcase
 end
